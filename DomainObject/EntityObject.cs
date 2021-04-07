fff2d56fd04fffa28b3c8cd6dbc8ae58cb3955a8
Joffrey Truyen <jtruyen@gismartware.com>
2021-04-07 09:37:02 +0200

    Initial commit

---
+++ DomainObject/EntityObject.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DomainObject
+{
+    public class EntityObject<TId> : IEquatable<EntityObject<TId>>
+    {
+        private readonly TId _id;
+
+        protected EntityObject(TId id)
+        {
+            if (object.Equals(id, default(TId)))
+            {
+                throw new ArgumentException("Id is default value");
+            }
+
+            _id = id;
+        }
+
+        public TId Id => _id;
+
+        public override bool Equals(object obj)
+        {
+            if (obj is EntityObject<TId> entity)
+            {
+                return this.Equals(entity);
+            }
+
+            return false;
+        }
+
+        public bool Equals(EntityObject<TId> other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return this.Id.Equals(other.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+    }
+}
---
+++ DomainObjectTests/ValueObjectTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace DomainObject.Tests
+{
+    [TestClass()]
+    public class ValueObjectTests
+    {
+        [TestMethod()]
+        public void EqualsTest_DifferentValues_ShouldFail()
+        {
+            Money five = new Money(5);
+            Money six = new Money(6);
+
+            Assert.AreNotEqual(five, six);
+        }
+
+        [TestMethod()]
+        public void EqualsTest_SameValues_ShouldSucceed()
+        {
+            Money five = new Money(5);
+            Money otherFive = new Money(5);
+
+            Assert.AreEqual(five, otherFive);
+        }
+    }
+
+    public class Money : ValueObject<Money>
+    {
+        public decimal Amount { get; private set; }
+
+        public Money(decimal amount)
+        {
+            Amount = amount;
+        }
+
+        protected override IEnumerable<object> GetAttributesToIncludeInEqualityCheck()
+        {
+            yield return Amount;
+        }
+    }
+}
